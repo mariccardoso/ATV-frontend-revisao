0b6eb0914cb34ce3bb276f907941cc4cd9cbfaaa
Mariana Cardoso <mariana.c.cardoso@aluno.senai.br>
2025-08-07 16:12:59 -0300

    feat: remove object fil cover on productImage

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -13,7 +13,7 @@ const ProductCard = ({ product }) => {
           style={{
             width: "100%",
             height: "100%",
-            objectFit:"cover",
+            // objectFit:"cover",
             
           }}
         />
